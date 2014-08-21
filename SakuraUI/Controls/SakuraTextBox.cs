c4dd563daabd65a67784658c0429b735b8622de9
Poisoner <yangwenyi@live.com>
2014-08-21 12:17:28 +0800

    Adding SakuraTextBox

---
+++ SakuraUI/Controls/SakuraTextBox.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace SakuraUI.Controls
+{
+    public sealed class SakuraTextBox : TextBox
+    {
+        public SakuraTextBox()
+        {
+            this.DefaultStyleKey = typeof(SakuraTextBox);
+        }
+    }
+}
